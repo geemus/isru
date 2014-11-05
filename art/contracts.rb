698ce5b21872399219353b28202128805cd160e3
geemus <geemus@gmail.com>
2014-11-04 20:51:26 -0600

    update contracts, simplify generation

--- art/contracts.rb
+++ art/contracts.rb
@@ -10,45 +10,74 @@ WIDTH     = 825
 
 require File.expand_path(File.join(DIRECTORY, 'art'))
 
-CONTRACTS = {
-  "5 Black"                     => 8,
-  "1 Black, 1 Grey and 1 White" => 8,
-  "3 Black and 1 White"         => 9,
-  "2 Grey and 1 White"          => 9,
-  "3 Black and 2 Grey"          => 10,
-  "4 Grey"                      => 10,
-  "3 White"                     => 12,
-  "3 Black, 2 Grey and 1 White" => 14
+COLORS = %w{
+  Black
+  Grey
+  White
 }
 
-CONTRACTS.each do |type,value|
-  [:circle, :diamond, :hexagon].each do |suit|
-    text = card('transparent')
+CONTRACTS = %w{
+  500
+  111
+  301
+  021
+  320
+  040
+  003
+  321
+}
+
+SUITS = %w{
+  circle
+  diamond
+  hexagon
+}
+
+CONTRACTS.each do |contract|
+  text, value = [], 0
+  contract.split('').each_with_index do |count, index|
+    next if count == "0"
+    text << "#{count} #{COLORS[index]}"
+    case COLORS[index]
+    when "Black"
+      value += count.to_i * 1.333
+    when "Grey"
+      value += count.to_i * 2.5
+    when "White"
+      value += count.to_i * 4
+    end
+  end
+  text = text.join (' + ')
+  value = value.ceil
+  value = value.to_s
+
+  SUITS.each do |suit|
+    name = "#{contract}#{suit[0,1]}=#{value}"
+
+    image = card('transparent')
 
     draw = Magick::Draw.new
-    draw.annotate(text, 0,0, PADDING, PADDING, type) do
+    draw.annotate(image, 0,0, PADDING, PADDING, text) do
       shared_text_settings(self)
       self.pointsize  = 60
       self.rotation   = 90
     end
-    draw.annotate(text, 0,0, PADDING, HEIGHT - PADDING, value.to_s) do
+    draw.annotate(image, 0,0, PADDING, HEIGHT - PADDING, value) do
       shared_text_settings(self)
       self.align      = Magick::RightAlign
       self.pointsize  = 60
       self.rotation   = 90
     end
 
-    flip_flop = text.dup.flip!.flop!
-    texts = text.composite(flip_flop, 0, 0, Magick::SrcOverCompositeOp)
+    flip_flop = image.dup.flip!.flop!
+    image = image.composite(flip_flop, 0, 0, Magick::SrcOverCompositeOp)
 
-    send(suit, texts)
+    send(suit.to_sym, image)
 
-    composite = card.composite(texts, 0, 0, Magick::SrcOverCompositeOp)
+    composite = card.composite(image, 0, 0, Magick::SrcOverCompositeOp)
     composite.level(0, Magick::QuantumRange, 1.5)
-    name = "#{type.gsub(/[\s,]/, '')}-#{suit}"
     composite.write("#{DIRECTORY}/../assets/contracts/#{name}.png")
   end
 end
 
-# Back
-card_back('Contracts', "#{DIRECTORY}/../assets/contracts/Contracts.png")
+card_back('Contracts', "#{DIRECTORY}/../assets/contracts/back.png")
